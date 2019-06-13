65cdb7207102304b4401450260d1bec323a562c3
felipe-abdalla <44029798+felipe-abdalla@users.noreply.github.com>
2019-06-13 01:05:16 -0300

    Objetos Destrutíveis
    Adicionei um script pra desativar o objeto e instanciar o FX qnd a personagem socar ele. Diminui o raio do colider da mão pra que não acione o script só andando pra cima do objeto, adicionei os objetos de madeira da cena como prefab com rigidbody e box collider já ajustados e com FX na referência do script. As mudanças na cena são pra que os projetos pareçam diferentes

---
+++ Assets/Scripts/DestructableOBJ.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableOBJ : MonoBehaviour
+{
+    public GameObject fxDestroy;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Character1_LeftHand" || other.gameObject.name == "Character1_RightHand")
+        {
+            gameObject.SetActive(false);
+            Instantiate(fxDestroy, transform.position, Quaternion.identity);
+        }
+    }
+}
